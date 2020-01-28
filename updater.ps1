87de73277805f0e0ef5a8c0e8a0c854d43cc88a7
zeffy <zeffy@users.noreply.github.com>
2020-01-28 06:58:11 -0800

    catch WebException [skip ci]

--- updater.ps1
+++ updater.ps1
@@ -7,20 +7,27 @@ if ( !$err `
         -or ([System.Version]$release.tag_name -gt [System.Version](Get-Content 'dnscrypt-proxy.tag'))) ) {
 
     if ( $asset = $release.assets | ? { $_.name -ilike 'dnscrypt-proxy-win64-*.zip' } ) {
-        $wc = [System.Net.WebClient]::new()
-        $wc.DownloadFile($asset.browser_download_url, $asset.name)
-        $wc.DownloadFile($asset.browser_download_url + '.minisig', $asset.name + '.minisig')
-        $wc.Dispose()
-        & '.\minisign.exe' -Vm "$($asset.name)" -P 'RWTk1xXqcTODeYttYMCMLo0YJHaFEHn7a3akqHlb/7QvIQXHVPxKbjB5' -q
-        if ( $? ) {
-            Expand-Archive -Path $asset.name -DestinationPath . -Force
-            Remove-Item -Path $asset.name, $($asset.name + '.minisig')
-            $service | Stop-Service
-            Get-ChildItem -Path 'win64\' | Move-Item -Destination . -Force
-            $service | Start-Service
-            Remove-Item -Path 'win64\'
-            $release.tag_name > 'dnscrypt-proxy.tag'
+        try {
+            $wc = [System.Net.WebClient]::new()
+            $wc.DownloadFile($asset.browser_download_url, $asset.name)
+            $wc.DownloadFile($asset.browser_download_url + '.minisig', $asset.name + '.minisig')
+            & '.\minisign.exe' -Vm "$($asset.name)" -P 'RWTk1xXqcTODeYttYMCMLo0YJHaFEHn7a3akqHlb/7QvIQXHVPxKbjB5' -q
+            if ( $? ) {
+                Expand-Archive -Path $asset.name -DestinationPath . -Force
+                Remove-Item -Path $asset.name, $($asset.name + '.minisig')
+                $service | Stop-Service
+                Get-ChildItem -Path 'win64\' | Move-Item -Destination . -Force
+                $service | Start-Service
+                Remove-Item -Path 'win64\'
+                $release.tag_name > 'dnscrypt-proxy.tag'
+            }
+        } catch [System.Net.WebException] {
+        } finally {
+            if ( $wc ) {
+                $wc.Dispose()
+            }
         }
+        
     }
 }
 $release = Invoke-RestMethod 'https://api.github.com/repos/zeffy/dnscrypt-lists/releases/latest' -ErrorVariable err
@@ -29,13 +36,20 @@ if ( !$err `
         -or ([int]$release.tag_name -gt [int](Get-Content 'dnscrypt-lists.tag'))) ) {
 
     $release.assets | ? { @('whitelist.zip', 'advanced-blacklist.zip') -icontains $_.name } | % {
-        $wc = [System.Net.WebClient]::new()
-        $wc.DownloadFile($_.browser_download_url, $_.name)
-        $wc.Dispose()
-        $service | Stop-Service
-        Expand-Archive -Path $_.name -DestinationPath . -Force
-        $service | Start-Service
-        Remove-Item -Path $_.name
-        $release.tag_name > "dnscrypt-lists.tag"
+        try {
+            $wc = [System.Net.WebClient]::new()
+            $wc.DownloadFile($_.browser_download_url, $_.name)
+            $wc.Dispose()
+            $service | Stop-Service
+            Expand-Archive -Path $_.name -DestinationPath . -Force
+            $service | Start-Service
+            Remove-Item -Path $_.name
+            $release.tag_name > "dnscrypt-lists.tag"
+        } catch [System.Net.WebException] {
+        } finally {
+            if ( $wc ) {
+                $wc.Dispose()
+            }
+        }
     }
 }
