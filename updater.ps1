3120d06baefe768ce0b96766e6da1fb79e63098a
zeffy <zeffy@users.noreply.github.com>
2020-01-28 21:17:28 -0800

    work around file lock issue [skip ci]

--- updater.ps1
+++ updater.ps1
@@ -16,6 +16,8 @@ if ( !$err `
                 Expand-Archive -Path $asset.name -DestinationPath . -Force
                 Remove-Item -Path $asset.name, $($asset.name + '.minisig')
                 $service | Stop-Service
+                $serivice.WaitForStatus([System.ServiceProcess.ServiceControllerStatus]::Stopped)
+                Start-Sleep -Seconds 3
                 Get-ChildItem -Path 'win64\' | Move-Item -Destination . -Force
                 $service | Start-Service
                 Remove-Item -Path 'win64\'
@@ -41,6 +43,8 @@ if ( !$err `
             $wc.DownloadFile($_.browser_download_url, $_.name)
             $wc.Dispose()
             $service | Stop-Service
+            $serivice.WaitForStatus([System.ServiceProcess.ServiceControllerStatus]::Stopped)
+            Start-Sleep -Seconds 3
             Expand-Archive -Path $_.name -DestinationPath . -Force
             $service | Start-Service
             Remove-Item -Path $_.name
